ced016e9cf1ff74174fa644ff1a5eedb7c4f78b4
zolwiastyl <jakub.zolw.kwiatek@gmail.com>
2023-10-31 07:10:49 +0100

    Add validation of strings and move things around

--- src/file_utils/config_file.rs
+++ src/file_utils/config_file.rs
@@ -1,41 +1,6 @@
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
-use serde::{Deserialize, Serialize};
-
-struct ConfigFile {}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GitConfig {
-    commit_format: String,
-    branch_format: String,
-}
-
-impl GitConfig {
-    fn default_config() -> Self {
-        return GitConfig {
-            commit_format: "".to_string(),
-            branch_format: "".to_string(),
-        };
-    }
-    fn from_file(path_to_file: PathBuf) -> Self {
-        if fs::metadata(&path_to_file).is_ok() {
-            let contents = fs::read_to_string(&path_to_file);
-            let contents = contents
-                .unwrap_or("{\"commit_format\": \"\", \"branch_format\": \"\"}".to_string());
-            let data = serde_json::from_str(&contents);
-            data.unwrap_or(Self::default_config())
-        } else {
-            Self::default_config()
-        }
-    }
-}
-
-pub fn open(custom_path_to_config_file: Option<PathBuf>) -> GitConfig {
-    let path_to_config = get_path_to_config(custom_path_to_config_file);
-    GitConfig::from_file(path_to_config)
-}
-
-fn get_path_to_config(path: Option<PathBuf>) -> PathBuf {
+pub fn get_path_to_config(path: Option<PathBuf>) -> PathBuf {
     if let Some(path) = path {
         return path;
     }
---
+++ src/git_config.rs
@@ -0,0 +1,49 @@
+use std::{fs, path::PathBuf};
+
+use crate::file_utils::config_file::get_path_to_config;
+use anyhow::{Error, Result};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+
+pub fn open(custom_path_to_config_file: Option<PathBuf>) -> GitConfig {
+    let path_to_config = get_path_to_config(custom_path_to_config_file);
+    GitConfig::from_file(path_to_config)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GitConfig {
+    commit_format: String,
+    branch_format: String,
+}
+
+impl GitConfig {
+    fn default_config() -> Self {
+        return GitConfig {
+            commit_format: "".to_string(),
+            branch_format: "".to_string(),
+        };
+    }
+    pub fn from_file(path_to_file: PathBuf) -> Self {
+        if fs::metadata(&path_to_file).is_ok() {
+            let contents = fs::read_to_string(&path_to_file);
+            let contents = contents
+                .unwrap_or("{\"commit_format\": \"\", \"branch_format\": \"\"}".to_string());
+            let data = serde_json::from_str(&contents);
+            data.unwrap_or(Self::default_config())
+        } else {
+            Self::default_config()
+        }
+    }
+    pub fn validate_against_interpolation_regex<'a>(
+        string_to_interpolate: &'a String,
+        name_of_field_to_check: &'static str,
+    ) -> Result<&'a String> {
+        let interpolation_regex = Regex::new(r"\{.*?\}").unwrap();
+        if interpolation_regex.is_match(string_to_interpolate) {
+            return Ok(string_to_interpolate);
+        };
+        return Err(Error::msg(format!(
+            "There was no interpolation signature {{}} introduced in {name_of_field_to_check}"
+        )));
+    }
+}
