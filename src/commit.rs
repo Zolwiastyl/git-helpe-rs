ab74130d5d32d4afd8d082894085a0e366e09d01
zolwiastyl <jakub.zolw.kwiatek@gmail.com>
2024-02-01 15:11:07 +0100

    remove some unused code

--- src/commit.rs
+++ src/commit.rs
@@ -1,20 +1,15 @@
 use core::panic;
 use std::process::Command;
-use std::{fmt::Debug, process::Stdio};
+use std::process::Stdio;
 
 use anyhow::{Error, Result};
 
 use crate::cli::DryRunAndCopyFlag;
 use crate::{
     cli::CommitOperationArguments,
-    git_config::{self, GitConfig},
+    git_config::GitConfig,
     template::{interpolate, validate_interpolation_places_count},
 };
-
-struct CommandPrintExecute {
-    print: String,
-    execute: Command,
-}
 
 enum RunMode {
     Normal,
@@ -106,66 +101,14 @@ pub fn commit_with_formatted_message(
                 .spawn()
                 .unwrap();
 
-            let copy = Command::new(copy_command)
+            Command::new(copy_command)
                 .stdin(Stdio::from(echo.stdout.unwrap()))
                 .output()
                 .unwrap();
+
             Ok(())
         }
     };
-
-    //     let mut cmd = Command::new("git");
-    //     let copy_command = config.data.clipboard_commands.copy;
-    //     let mut echo_cmd = Command::new("echo");
-    //     let mut copy_cmd = Command::new(copy_command);
-
-    //     let command_to_execute = cmd.arg("commit").arg("-m").arg(interpolated_commit);
-
-    //     let command_to_execute: CommandPrintExecute = if options.flags.copy {
-
-    //         let value_to_copy = remove_quotes_from_string(format!("{:?}", command_to_execute));
-    //         println!("{}", value_to_copy);
-
-    // let echo_output = echo_cmd
-    //         .stdout(Stdio::piped())       // of which we will pipe the output.
-    //         .spawn()                      // Once configured, we actually spawn the command...
-    //         .unwrap();
-
-    //     let echo = Command::new("echo").arg()
-    //         let copy_command = copy_cmd
-    //             .arg(format!("'{}'", value_to_copy))
-    //             .arg(">")
-    //             .arg(copy_command);
-    //         copy_command
-
-    //     } else {
-    //         command_to_execute
-    //     };
-
-    //     return if options.flags.dry_run {
-    //         let command_to_print = remove_quotes_from_string(format!("{:?}", command_to_execute));
-    //         println!("Command that's going to be run: \n {:?}", command_to_print);
-    //         Ok(())
-    //     } else {
-    //         let output = command_to_execute
-    //             .output()
-    //             .expect("failed to run")
-    //             // .unwrap()
-    //             .stdout;
-    //         println!("{}", String::from_utf8_lossy(&output));
-    //         Ok(())
-    //     };
-
-    //     if (options.flags.copy) {
-    //         // Copy
-    //         todo!("Copy")
-    //     } else {
-    //         // Run
-    //         let output = command_to_execute.output().unwrap().stdout;
-
-    //         println!("{}", String::from_utf8_lossy(&output));
-    //         Ok(())
-    //     }
 }
 
 fn get_run_mode_from_options(flags: DryRunAndCopyFlag) -> RunMode {
@@ -181,47 +124,5 @@ fn get_run_mode_from_options(flags: DryRunAndCopyFlag) -> RunMode {
         } else {
             RunMode::Normal
         }
-    }
-}
-
-fn remove_quotes_from_string(str: String) -> String {
-    let pattern = format!("{}", "\"");
-    str.replace(&pattern, "")
-    // str
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn should_remove_at_the_beginning() {
-        let str = "\"git".to_owned();
-        let output = remove_quotes_from_string(str);
-        let expected = "git".to_owned();
-        assert_eq!(output, expected)
-    }
-
-    #[test]
-    fn should_remove_at_the_end() {
-        let str = "git\"".to_owned();
-        let output = remove_quotes_from_string(str);
-        let expected = "git".to_owned();
-        assert_eq!(output, expected)
-    }
-
-    #[test]
-    fn should_remove_on_both_places() {
-        let str = "\"git\"".to_owned();
-        let output = remove_quotes_from_string(str);
-        let expected = "git".to_owned();
-        assert_eq!(output, expected)
-    }
-
-    #[test]
-    fn should_remove_on_multiple_places() {
-        let str = "\"git\" \"hit jit\"".to_owned();
-        let output = remove_quotes_from_string(str);
-        let expected = "git hit jit".to_owned();
-        assert_eq!(output, expected)
     }
 }
